31f6464dfc9bda6ce15034e3904e8633554bd7bd
yassinedehbi <55984069+yassinedehbi@users.noreply.github.com>
2024-09-10 16:29:37 +0200

    Update mytable1.sql [skip-ci]

--- db/myschema/Tables/mytable1.sql
+++ db/myschema/Tables/mytable1.sql
@@ -1,4 +1,4 @@
-create or replace TABLE db_{{enviroment}}.MYSCHEMA.MYTABLE1 (
+create or replace TABLE db_{{environment}}.MYSCHEMA.MYTABLE1 (
 	COL1 NUMBER(38,0),
 	COL2 NUMBER(38,0),
 	COL3 NUMBER(38,0),
