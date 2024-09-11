f211edd057873e677092fb4353705c6eae73c1d6
yassinedehbi <yassinedehbi4@gmail.com>
2024-09-11 14:25:12 +0200

    alter [skip-cd]

--- db/myschema/Tables/mytable1.sql
+++ db/myschema/Tables/mytable1.sql
@@ -1,4 +1,4 @@
-create or replace TABLE db_{{environment}}.MYSCHEMA.MYTABLE1 (
+create or alter TABLE db_{{environment}}.MYSCHEMA.MYTABLE1 (
 	COL1 NUMBER(38,0),
 	COL2 NUMBER(38,0),
 	COL3 NUMBER(38,0),
